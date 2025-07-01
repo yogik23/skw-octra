0ace58af5a776efbdcdabc77bb85cc303eecc81f
yogik23 <61941105+yogik23@users.noreply.github.com>
2025-07-01 23:17:58 +0700

    Create config.js

---
+++ skw/config.js
@@ -0,0 +1,24 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+export const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+export const rpcUrl = 'https://octra.network';
+
+export const privateKeys = fs.readFileSync(path.join(__dirname, "..privatekey.txt"), "utf-8")
+  .split("\n")
+  .map(k => k.trim())
+  .filter(k => k.length > 0);
+
+export const Recepient = fs.readFileSync(path.join(__dirname, "..recepient.txt"), "utf-8")
+  .split("\n")
+  .map(k => k.trim())
+  .filter(k => k.length > 0);
+
+export function RandomAmount(min, max, decimalPlaces) {
+  return (Math.random() * (max - min) + min).toFixed(decimalPlaces);
+}
+
+export function randomdelay(min = 10000, max = 20000) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
